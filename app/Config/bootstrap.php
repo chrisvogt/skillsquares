dbe235815b2be48b18efa59ebc3b2a321482469d
Chris Vogt <mail@chrisvogt.me>
2013-03-02 01:24:02 -0700

    Connect DebugKit.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -27,6 +27,9 @@
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 
+// Include DebugKit
+CakePlugin::load('DebugKit');
+
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
  *
